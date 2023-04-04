91f6bb23a2ba6fa3322a190ea44e57ac7c7451d5
alex.schulster <zanfoin@hotmail.fr>
2023-04-04 22:26:22 +0200

    Corrected indent

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -40,10 +40,10 @@ local cmp = require('cmp')
 
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
-  ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
-  ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+    ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ["<C-Space>"] = cmp.mapping.complete(),
 })
 
 cmp.setup({
@@ -56,13 +56,13 @@ cmp.setup({
         { name = "nvim_lsp" },  -- LSP
         { name = 'luasnip' },   -- Snippets
         { name = "path" },      -- Path
-      -- more sources
+        -- more sources
     },
 })
 
 -- Set key bindings
 lsp.setup_nvim_cmp({
-  mapping = cmp_mappings
+    mapping = cmp_mappings
 })
 
 -- Set up some LSP settings
@@ -78,16 +78,16 @@ lsp.set_preferences({
 
 -- Define function that will be executed when attaching an LSP to a buffer
 lsp.on_attach(function(_, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
 
-  vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
-
-  vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
-  vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.references() end, opts)
-  vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
-  vim.keymap.set("n", "<leader>ln", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "<leader>lN", function() vim.diagnostic.goto_prev() end, opts)
+    -- Set remaps
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>ln", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "<leader>lN", function() vim.diagnostic.goto_prev() end, opts)
 end)
 
 -- (Optional) Configure lua language server for neovim
