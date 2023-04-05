6a8e70d75d2ebe68cdeb17a2a376a42b693ff738
alex.schulster <zanfoin@hotmail.fr>
2023-04-05 19:54:20 +0200

    Modified telescope settings to quit in a single escape key press

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,3 +7,17 @@
 -- Shortcut `builtin` command
 require('telescope.builtin')
 
+-- Load actions
+local actions = require("telescope.actions")
+
+-- Set mapping to quit telescope with a single escape key
+require("telescope").setup({
+    defaults = {
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close,
+            },
+        },
+    },
+})
+
