43644d4abe8cf13d83af0c250871fdcb8873ee52
alex.schulster <zanfoin@hotmail.fr>
2023-04-06 22:35:20 +0200

    Added plugin for easy text surrounding

--- lua/alex/lazy.lua
+++ lua/alex/lazy.lua
@@ -137,6 +137,18 @@ Plugins = {
 
     -- Illuminate the word under the curser and its references
     'RRethy/vim-illuminate',
+
+    -- Surround feature
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
 }
 
 -- Call lazy
