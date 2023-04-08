a28ee0bf1083341ddb4d9fc17e3777c724d6d6d0
alex.schulster <zanfoin@hotmail.fr>
2023-04-08 12:54:12 +0200

    Removed key remaps repeats

--- lua/alex/remap.lua
+++ lua/alex/remap.lua
@@ -6,9 +6,6 @@
 -- Set Vim Leader
 vim.g.mapleader = " "
 
--- Launch Lazy Vim
-vim.keymap.set("n", "<leader>l", vim.cmd.Lazy)
-
 -- Move selected text in visual mode up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
@@ -20,14 +17,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Scrolling half page up
 vim.keymap.set("n", "n", "nzzzv")       -- Finding next occurence of search
 vim.keymap.set("n", "N", "Nzzzv")       -- Finding previous occurence of search
 
--- Quick actions for better yank / paste
-vim.keymap.set("x", "<leader>p", [["_dP]])       -- Paste without yanking deletion
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- Yank in systemp clipboard
-vim.keymap.set("n", "<leader>Y", [["+Y]])        -- Same
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- Delete in void register
--- Make current file executable
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
 -- Navigate between splits
 vim.keymap.set("n", "<C-h>", "<C-w>h")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
@@ -36,8 +25,4 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Ctrl-a for whole buffer selection
 vim.keymap.set("n", "<C-a>", "ggVG")
-vim.keymap.set("n", "<leader>=", function ()
-    local r, _ = unpack(vim.api.nvim_win_get_cursor(0))
-    vim.api.nvim_feedkeys(string.format("ggVG=%sgg", r), 'n', true)
-end)
 
