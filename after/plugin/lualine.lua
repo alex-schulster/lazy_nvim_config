211aade5d50a8082fb5ebc4878ff020707fc170a
alex.schulster <zanfoin@hotmail.fr>
2023-04-10 13:58:12 +0200

    Removed file format from lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -27,7 +27,7 @@ require("lualine").setup({
         lualine_a = {'mode'},
         lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {'filename', require("lsp-progress").progress},
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
+        lualine_x = {'encoding', 'filetype'},
         lualine_y = {'progress'},
         lualine_z = {'location'}
     },
