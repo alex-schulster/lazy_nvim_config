dd9207718ca3adcb36676d5895f3bbbc09e85b39
alex.schulster <zanfoin@hotmail.fr>
2023-09-18 09:55:37 +0200

    Added ltex language server for latex document redaction

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -49,6 +49,22 @@ lsp.on_attach(function(client, bufnr)
     -- Attach breadcrumbs
     if client["name"] ~= 'ltex' then
         require("nvim-navbuddy").attach(client, bufnr)
+    else
+        require("ltex_extra").setup(
+        {
+            -- https://valentjn.github.io/ltex/supported-languages.html#natural-languages
+            load_langs = { "en-US", "fr"}, -- en-US as default
+
+            -- boolean : whether to load dictionaries on startup
+            init_check = true,
+
+            -- string : relative or absolute path to store dictionaries
+            path = ".ltex", -- project root or current working directory
+
+            -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
+            log_level = "none",
+        }
+        )
     end
 
     -- Attach lsp signature
