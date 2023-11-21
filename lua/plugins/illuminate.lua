32bafff9619016c2fc248ea479de75d8ac5ec762
Alex Schulster <schulsteralex@gmail.com>
2023-11-21 13:07:12 +0100

    feat(illuminate): Add file size criteria for enable / disable
    In addition to line numbers, chech if the file is not too big to enable vim illuminate

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -12,7 +12,15 @@ illuminate.configure({
     -- delay: delay in milliseconds
     delay = 0,
     -- Add cutoff to 25 000 lines to ignore file that are longer
-    large_file_cutoff = 25000
+    large_file_cutoff = 25000,
+    -- should_enable: a callback that overrides all other settings to
+    -- enable/disable illumination. This will be called a lot so don't do
+    -- anything expensive in it.
+    should_enable = function(bufnr)
+        local max_file_size = 100 * 1024 -- 100 kB in bytes
+        local file_size = vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr))
+        return file_size < max_file_size
+    end,
 })
 
 -- Define remaps
